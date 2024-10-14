607e8567a666a445e3cdada544c9d3b3a7326643
NaeemRaza16 <naeemraza2916@gmail.com>
2024-10-14 06:31:54 +0500

    some work

---
+++ src/components/CustomizeProduct.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomizeProduct = () => {
+  return (
+    <div>CustomizeProduct</div>
+  )
+}
+
+export default CustomizeProduct
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -5,7 +5,10 @@ import React from "react";
 const ProductList = () => {
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
         <div className="relative w-full h-80">
           <Image
             src="https://images.pexels.com/photos/27605801/pexels-photo-27605801/free-photo-of-close-up-of-a-pile-of-lemons.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
@@ -23,11 +26,100 @@ const ProductList = () => {
           />
         </div>
         <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$49</span>
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
         </div>
         <div className="text-sm text-gray-500">My description</div>
-        <button className="rounded-2xl ring-1 ring-raza text-raza"></button>
+        <button className="rounded-2xl ring-1 ring-raza text-raza w-max py-2 px-4 text-xs hover:bg-raza hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/27605801/pexels-photo-27605801/free-photo-of-close-up-of-a-pile-of-lemons.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/25189329/pexels-photo-25189329/free-photo-of-sweet-cupcake-with-cream.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-raza text-raza w-max py-2 px-4 text-xs hover:bg-raza hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/27605801/pexels-photo-27605801/free-photo-of-close-up-of-a-pile-of-lemons.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/25189329/pexels-photo-25189329/free-photo-of-sweet-cupcake-with-cream.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-raza text-raza w-max py-2 px-4 text-xs hover:bg-raza hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/27605801/pexels-photo-27605801/free-photo-of-close-up-of-a-pile-of-lemons.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/25189329/pexels-photo-25189329/free-photo-of-sweet-cupcake-with-cream.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-raza text-raza w-max py-2 px-4 text-xs hover:bg-raza hover:text-white">
+          Add to Cart
+        </button>
       </Link>
     </div>
   );
