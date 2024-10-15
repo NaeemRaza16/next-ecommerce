ee1cdba66020ad55bcf34a0781e8cd0615111c13
NaeemRaza16 <naeemraza2916@gmail.com>
2024-10-15 18:09:33 +0500

    2.38 hours done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,42 @@
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import { WixClientContext } from "@/context/wixContent";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense, useContext, useEffect } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  // const wixClient = useWixClient()
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+  //     console.log(res)
+  //   };
+
+  //   getProducts()
+  // }, [wixClient]);
+
+
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={"Loading..."}>
+          <ProductList categoryId={process.env.FEATURED_PRODUCT_CATEGORY_ID!} limit={4} />
+        </Suspense>
       </div>
       <div className="mt-24">
-        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
         <CategoryList />
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
+        {/* <ProductList /> */}
       </div>
     </div>
   );
---
+++ src/hooks/useWixClient.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { WixClientContext } from "@/context/wixContent";
+import { useContext } from "react";
+
+export const useWixClient = () => {
+  return useContext(WixClientContext);
+};
