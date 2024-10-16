3a1a67e46f8a9677f1c096542257746ad30e90dc
NaeemRaza16 <naeemraza2916@gmail.com>
2024-10-16 17:49:20 +0500

    3.27 hour done

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,28 +1,53 @@
 import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
-import DOMPurify from "isomorphic-dompurify";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import DOMPurify from "isomorphic-dompurify";
+// import Pagination from "./Pagination";
 
-const PRODUCT_PER_PAGE = 20;
+const PRODUCT_PER_PAGE = 8;
 
 const ProductList = async ({
   categoryId,
   limit,
+  searchParams,
 }: {
   categoryId: string;
   limit?: number;
+  searchParams?: any;
 }) => {
   const wixClient = await wixClientServer();
 
-  const res = await wixClient.products
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith("name", searchParams?.name || "")
     .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
     .limit(limit || PRODUCT_PER_PAGE)
-    .find();
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
+  // .find();
 
-  console.log(res);
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
@@ -42,7 +67,7 @@ const ProductList = async ({
             />
             {product.media?.items && (
               <Image
-                src={product.media?.items[1].image?.url || "/product.png"}
+                src={product.media?.items[1]?.image?.url || "/product.png"}
                 alt=""
                 fill
                 sizes="25vw"
@@ -66,11 +91,18 @@ const ProductList = async ({
               }}
             ></div>
           )}
-          <button className="rounded-2xl ring-1 ring-raza text-raza w-max py-2 px-4 text-xs hover:bg-raza hover:text-white">
+          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
             Add to Cart
           </button>
         </Link>
       ))}
+      {/* {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null} */}
     </div>
   );
 };
